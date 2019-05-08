e4940775bfc4318eebeff69d1dfdb0c178d8e152
Paweł Bucior <pawel121111@gmail.com>
2019-05-08 21:37:21 +0200

    UserPanel fixing

--- UserPanel.xaml.cs
+++ UserPanel.xaml.cs
@@ -25,6 +25,16 @@ namespace CarRentalClient
             InitializeComponent();
         }
 
+        public void InitializeDataGrid()
+        {
+            List<CarReadyToRent> carReadyToRents;
+            carReadyToRents = JsonConvert.DeserializeObject<List<CarReadyToRent>>(GetCarsReadyToRent("http://localhost:8080/car/readytorent", Token));
+            myDataGrid.ItemsSource = carReadyToRents;
+            if (carReadyToRents.Count == 0)
+            {
+                rentCar.IsEnabled = false;
+            }
+        }
 
 
 
@@ -46,49 +56,6 @@ namespace CarRentalClient
             
         }
 
-        public void InitializeDataGrid()
-        {
-            List<CarReadyToRent> carReadyToRents;
-            carReadyToRents = JsonConvert.DeserializeObject<List<CarReadyToRent>>(GetCarsReadyToRent("http://localhost:8080/car/readytorent", Token));
-            myDataGrid.ItemsSource = carReadyToRents;
-            if (carReadyToRents.Count == 0)
-            {
-                rentCar.IsEnabled = false;
-            }
-        }
-
-
-        static string GetCarsReadyToRent(string url, string Token)
-        {
-
-            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
-            using (var client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + Token);
-                var response = client.GetAsync(url).Result;
-
-                String responseee = response.Content.ReadAsStringAsync().Result;
-                return response.Content.ReadAsStringAsync().Result;
-            }
-
-
-        }
-
-
-
-
-        private void Button_Get_Rents(object sender, RoutedEventArgs e)
-        {
-            List<RentedCars> rentedCars;
-            rentedCars = JsonConvert.DeserializeObject<List<RentedCars>>(GetCarsReadyToRent("http://localhost:8080/rent/", Token));
-            dataGridRentings.ItemsSource = rentedCars;
-        }
-
-        private void Button_Return_Car(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void Button_Rent(object sender, RoutedEventArgs e)
         {
 
@@ -103,12 +70,12 @@ namespace CarRentalClient
             carReadyToRents = JsonConvert.DeserializeObject<List<CarReadyToRent>>(GetCarsReadyToRent("http://localhost:8080/car/readytorent", Token));
             long id;
             String json = "{" +
-                "\"rentEndDate\": \""+dateTime.ToString("yyyy-MM-dd")+ "T00:00:00.000Z" + "\"" +
+                "\"rentEndDate\": \"" + dateTime.ToString("yyyy-MM-dd") + "T00:00:00.000Z" + "\"" +
                 "}";
 
-            foreach(CarReadyToRent carReadyToRent in carReadyToRents)
+            foreach (CarReadyToRent carReadyToRent in carReadyToRents)
             {
-                if(carReadyToRent.Mark +" "+ carReadyToRent.Model == car)
+                if (carReadyToRent.Mark + " " + carReadyToRent.Model == car)
                 {
                     id = carReadyToRent.ID;
                     PostRentCar("http://localhost:8080/rent/car/" + id, Token, json);
@@ -120,9 +87,43 @@ namespace CarRentalClient
                 }
             }
             mainMenu.IsSelected = true;
+            rentTab.IsEnabled = false;
 
 
         }
+
+
+
+
+
+
+
+        private void Button_Get_Rents(object sender, RoutedEventArgs e)
+        {
+            allRentedCars.IsEnabled = true;
+            allRentedCars.IsSelected = true;
+            List<RentedCars> rentedCars;
+            rentedCars = JsonConvert.DeserializeObject<List<RentedCars>>(GetCarsReadyToRent("http://localhost:8080/rent/", Token));
+            dataGridRentings.ItemsSource = rentedCars;
+        }
+
+
+
+        private void Button_Return_Car(object sender, RoutedEventArgs e)
+        {
+            returnCarTab.IsEnabled = true;
+            returnCarTab.IsSelected = true;
+            List<RentedCars> rentedCars;
+            rentedCars = JsonConvert.DeserializeObject<List<RentedCars>>(GetCarsReadyToRent("http://localhost:8080/rent/", Token));
+            foreach(RentedCars rentedCar in rentedCars)
+            {
+                if(rentedCar.RentEndDate>DateTimeOffset.Now)
+                comboBoxReturnCar.Items.Add(rentedCar.Mark + " " + rentedCar.Model + " " + rentedCar.RegisterName);
+            }
+
+        }
+
+      
        
 
         private void PostRentCar(String url, String token,String json)
@@ -143,6 +144,66 @@ namespace CarRentalClient
                 var result = streamReader.ReadToEnd();
             }
             
+        }
+        static string GetCarsReadyToRent(string url, string Token)
+        {
+
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + Token);
+                var response = client.GetAsync(url).Result;
+
+                String responseee = response.Content.ReadAsStringAsync().Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+
+
+        }
+        static string DeleteRent(string url, string Token)
+        {
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + Token);
+                var response = client.DeleteAsync(url).Result;
+                String responseee = response.Content.ReadAsStringAsync().Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+        }
+
+        private void ReturnMainMenu_Click(object sender, RoutedEventArgs e)
+        {
+            mainMenu.IsSelected = true;
+            allRentedCars.IsEnabled = false;
+            InitializeDataGrid();
+        }
+
+        private void ButtonReturnCar_Click(object sender, RoutedEventArgs e)
+        {
+            if (comboBoxReturnCar.SelectedItem != null)
+            {
+                String car = comboBoxReturnCar.SelectedItem.ToString();
+           
+            long id;
+            List<RentedCars> rentedCars;
+            rentedCars = JsonConvert.DeserializeObject<List<RentedCars>>(GetCarsReadyToRent("http://localhost:8080/rent/", Token));
+            foreach (RentedCars rentedCar in rentedCars)
+            {
+                if(rentedCar.Mark + " " + rentedCar.Model + " " + rentedCar.RegisterName == car)
+                {
+                    id = rentedCar.IDRent;
+                    DeleteRent("http://localhost:8080/rent/return/" + id, Token);
+                    mainMenu.IsSelected = true;
+                    comboBoxReturnCar.Items.Clear();
+                    returnCarTab.IsEnabled = false;
+                    break;
+                }
+            }
+                InitializeDataGrid();
+            }
+            else
+                MessageBox.Show("You have to select some car");
         }
     }
 }
